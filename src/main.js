5f958e82e259de497ae77d308402b827eba10c30
Henry <Henrylok098@gmail.com>
2026-09-03 21:45:32 -0400

    feat: darkmode

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('login-container');
 const signInBtn = document.getElementById('btn-sign-in');
 const signUpBtn = document.getElementById('btn-sign-up');
+const darkBtn = document.getElementById('theme-toggle');
 
 signUpBtn.addEventListener('click', function () {
     container.classList.add('active');
@@ -10,4 +11,12 @@ signInBtn.addEventListener('click', function (
 ) {
     container.classList.remove('active');
     container.classList.add('was-active');
+});
+
+darkBtn.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+
+    const icon = darkBtn.querySelector('i');
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
 });
