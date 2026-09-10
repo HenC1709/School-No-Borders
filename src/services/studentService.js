599a55ea80868c161956393f645c64e299e7a9d5
Henry <Henrylok098@gmail.com>
2026-09-09 22:29:16 -0400

    feat: proto auth service

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,5 +1,25 @@
+// obtener todos los estudiantes
 async function getStudents() {
+    // marcamos ruta de archivo
     const response = await fetch('src/data/Students.json');
     const data = await response.json();
     return data;
+}
+
+// funcion de encontrar DNI
+async function findStudentByDni(dni) {
+
+    // llamamos a getStudents
+    const students =  await getStudents();
+
+    // usamos el find para devolver la condicion correcta
+
+    const studentFind = students.find(student => student.dni === dni);
+    return studentFind;
+}
+
+// funcion para el localStorage
+// futura evolucion.
+function getRegisteredStudents() { 
+    return JSON.parse(localStorage.getItem('registeredStudents') || '{}');
 }
